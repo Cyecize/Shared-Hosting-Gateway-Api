c77a7b519aeac41b243abd81dba837cd8bbff0dd
Tsvetelin Yakimov <ceci2205@abv.bg>
2022-07-15 16:40:47 +0300

    Add code from Part 1 - Spring Cloud Gateway

---
+++ part1-spring-clound/domain-router/src/main/java/com/cyecize/domainrouter/DomainRouterApplication.java
@@ -0,0 +1,13 @@
+package com.cyecize.domainrouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainRouterApplication.class, args);
+	}
+
+}
